c5c0341e05231cbf4d9890bdf64dc8ef49d9a285
siefBaher <seifbaher15@gmail.com>
2025-02-25 00:00:04 +0200

    Fix start script for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import "@fortawesome/fontawesome-free/css/all.min.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "flowbite/dist/flowbite.min.js";
+import "flowbite/dist/flowbite.min.js"
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
