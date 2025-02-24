3c990d4b972a7c969ebc24b05592ba3d9097f244
siefBaher <seifbaher15@gmail.com>
2025-02-24 22:43:04 +0200

    first commit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from "./Layout.module.css"
 import Navbar from './../Navbar/Navbar';
-import Footer from './../footer/footer';
+import Footer from './../Footer/Footer.jsx';
 import { Outlet } from 'react-router-dom';
 
 
